31237243a17b2c2e55d43a4acea6abc99545a61e
DL <den2033@bk.ru>
2023-10-11 17:30:05 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿int health = 5, maxHealth = 10;
+int mana = 7, maxMana = 10;
+
+DrawBar(health, maxHealth, ConsoleColor.Green, 0, '|');
+DrawBar(mana, maxMana, ConsoleColor.Blue, 1);
+
+static void DrawBar(int value, int maxValue, ConsoleColor color, int position, char symbol = ' ')
+{
+    ConsoleColor defaultColor = Console.BackgroundColor;
+
+    string bar = "";
+
+    for (int i = 0; i < value; i++)
+    {
+        bar += symbol;
+    }
+
+    Console.SetCursorPosition(0, position);
+    Console.Write('[');
+    Console.BackgroundColor = color;
+    Console.Write(bar);
+    Console.BackgroundColor = defaultColor;
+
+    bar = "";
+
+    for (int i = value; i < maxValue; i++)
+    {
+        bar += symbol;
+    }
+
+    Console.Write(bar + ']');
+}
